11b420d50f039f732b7ee9db33e5811818f13773
ilias <iliaschokairi@gmail.com>
2024-01-03 16:17:48 +0100

    try

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,18 +3,6 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    while (tree->left)
-    {
-        free(tree->left);
-        free(tree->left->right);
-        tree->left = tree->left->left;
-    }
-    while (tree->right)
-    {
-        free(tree->right);
-        free(tree->right->left);
-        tree->right = tree->right->right;
-    }
     
     free(tree);
 }
