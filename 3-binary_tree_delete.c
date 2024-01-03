a6971bc3ffe2e4d240504f7302b9205521608621
ilias <iliaschokairi@gmail.com>
2024-01-03 16:29:48 +0100

    free my tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,8 +1,14 @@
 #include <stdlib.h>
 #include "binary_trees.h"
-
+/**
+ * binary_tree_delete - function that free my node
+ * @tree: the root node
+*/
 void binary_tree_delete(binary_tree_t *tree)
 {
-    
-    free(tree);
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
