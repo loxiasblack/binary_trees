c62c1a2a242ccca62fd866dbca10c5997c4960a4
ilias <iliaschokairi@gmail.com>
2024-01-03 15:46:21 +0100

    fix null parent issue

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (!new_node || parent == NULL)
+	if (parent == NULL)
+		return (NULL);
+	if (!new_node)
 	{
 		return (NULL);
 	}
